7a750837309e046bec66da90f209e6b3eff9613b
mlp01kodehode <mlp01.kodehode@gmail.com>
2025-02-17 13:11:08 +0100

    Added temperature treshold

--- Program.cs
+++ Program.cs
@@ -4,11 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Leser en tekststreng som brukeren skriver inn i konsollen og konverterer den til en double (desimaltall)
         Console.WriteLine("Angi temperaturen i grader Celsius:");
         double temperatur = Convert.ToDouble(Console.ReadLine());
 
-        // Grenseverdier
-        double lowTreshold = 0,0d;
-        double highTreshold = 25,0d;
+        // Definer grenseverdier, obs. bruk punktum for desimaltall og ikke komma!
+        double lowTreshold = 0.0;
+        double highTreshold = 25.0;
+
+        // Sjekk temperaturen og gi info
+        if (temperatur < lowTreshold)
+        {
+            Console.WriteLine("Temperaturen er lav (under 0°C).");
+        }
+        else if (temperatur > highTreshold)
+        {
+            Console.WriteLine("Temperaturen er høy (over 25°C).");
+        }
+        else
+        {
+            Console.WriteLine("Temperaturen er innenfor normalområdet (0°C - 25°C).");    
+        }
     }
 }
