e2bad02e7eb2a0d045d97a8e842453c6456bf37e
mlp01kodehode <mlp01.kodehode@gmail.com>
2025-02-17 11:24:22 +0100

    start-project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TemperaturSjekker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Angi temperaturen i grader Celsius:");
+        double temperatur = Convert.ToDouble(Console.ReadLine());
+    }
+}
