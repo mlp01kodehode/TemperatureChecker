423577c30bd7e669bfc49d0880e57b5fa55c955f
mlp01kodehode <mlp01.kodehode@gmail.com>
2025-02-17 12:04:55 +0100

    Added temperature limits

--- Program.cs
+++ Program.cs
@@ -6,5 +6,9 @@ class Program
     {
         Console.WriteLine("Angi temperaturen i grader Celsius:");
         double temperatur = Convert.ToDouble(Console.ReadLine());
+
+        // Grenseverdier
+        double lowTreshold = 0,0d;
+        double highTreshold = 25,0d;
     }
 }
